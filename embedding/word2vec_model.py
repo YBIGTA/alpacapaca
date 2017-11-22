01febc94d6f1b7bed6d70c58d4e57a5d5748a29a
dreamgonfly <dreamgonfly@gmail.com>
2017-11-22 22:25:27 +0900

    [임베딩-003] Finetune Word2Vec model module
    - Deleted pickle package which we don't use
    - Don't put space before colon(:)
    - Seperate TextUtils from Word2VecModel class
    - Add build_embedding function which is like main function
    - Add __name__ == '__main__' part

--- embedding/word2vec_model.py
+++ embedding/word2vec_model.py
@@ -1,41 +1,52 @@
 from gensim.models import Word2Vec
-import pickle as p
 
-class Word2VecModel() :
-    def __init__(self, num_features, min_word_count, num_workers, context, downsampling, sg) :
-        self.num_features = num_features
-        self.min_word_count = min_word_count
-        self.num_workers = num_workers
-        self.context = context
-        self.downsampling = downsampling
-        self.sg = sg
-        
-        
-    def read_data(self, filename) :
-        with open(filename, 'r', encoding= "utf-8") as file :
+class TextUtils:
+    @staticmethod
+    def read_words(filename):
+        with open(filename, 'r', encoding= "utf-8") as file:
             sentences = file.readlines()
         words = []
         for sent in sentences :
             words.append(sent.strip().split())
         return words
-    
-    
-    def build_embedding(self, filename) :
-        words = self.read_data(filename)
-        model = Word2Vec(words, 
-                         num_features= self.num_features,
-                         min_word_count= self.min_word_count, 
-                         num_workers= self.num_workers,
-                         context= self.context, 
-                         downsample= self.downsampling, 
-                         sg = self.sg)
- 
-        model.init_sims(replace=True)
-        self.model = model
+
+class Word2VecModel() :
+    def __init__(self, num_features=500, min_word_count=10, num_workers=4, context=10, downsampling=1e-5, sg=1):
+        self.num_features = num_features
+        self.min_word_count = min_word_count
+        self.num_workers = num_workers
+        self.context = context # window size
+        self.downsampling = downsampling
+        # skip gram이 좋은 이유 : https://ratsgo.github.io/from%20frequency%20to%20semantics/2017/03/30/word2vec/
+        self.sg = sg
         
-    def save(self, filename):
-        self.model.save(filename)
+    def train_model(self, words):
+        self.model = Word2Vec(words, 
+                         num_features=self.num_features,
+                         min_word_count=self.min_word_count, 
+                         num_workers=self.num_workers,
+                         context=self.context, 
+                         downsample=self.downsampling, 
+                         sg=self.sg)
+  
+        # 더 train 시킬 생각 없을 때. 필요 없는 메모리를 unload 시킨다.
+        self.model.init_sims(replace=True)
+
+    def save(self, model_filename):
+        self.model.save(model_filename)
     
+    def load(self, model_filename):
+        self.model = Word2Vec.load(filemodel_filenamename)
+
+    def build_embedding(self, input_filename, model_filename):
+
+        self.words = TextUtils.read_words(input_filename)
+        self.train_model(self.words)
+        self.save(model_filename)
+
+if __name__ == '__main__':
     
-    def load(self, filename):
-        self.model = Word2Vec.load(filename)
+    input_filename = 'result.txt'
+    model_filename = 'word2vec_model'
+    model = Word2VecModel()    
+    model.build_embedding(input_filename=input_filename, model_filename=model_filename)
