ad21b728c9d8f5d04d0252adf2b2091d81ddc439
yunseunghyun <rogve22@gmail.com>
2017-12-06 23:00:57 +0900

    [임베이딩-003] sample embedding 추가, batch size 맞지 않는 경우 트레이닝 셋 zero padding

--- data_load/embedding_loader.py
+++ data_load/embedding_loader.py
@@ -6,23 +6,30 @@ import numpy as np
 
 class EmbeddingDataset(Dataset):
     
-    def __init__(self, filepath, embedding, vector_size):
-        
+    def __init__(self, filepath, embedding, vector_size, sampleSize):
+
         with open(filepath, 'r') as f:
-            lines = f.readlines()
-        
+            if sampleSize is not None:
+                lines = f.readlines()[1:sampleSize]
+            else:
+                lines = f.readlines()
+
         self.unknown = np.random.randn(vector_size)
         self.EOS = np.zeros(vector_size)
-        
+
         self.splitted = [line.strip().split() for line in lines]
-        self.cleaned = [line for line in self.splitted if len(line) > 3 and len(line) < 50] 
+        self.cleaned = [line for line in self.splitted if len(line) > 3 and len(line) < 50]
+        self.cleaned = self.cleaned[: int(len(self.cleaned) / 10) * 10]
         self.embedding = embedding
+        print('total read line %d' % len(lines))
+        print('cleaned lines %d' % len(self.cleaned))
         
     def __getitem__(self, index):
         line = self.cleaned[index]
-        
-        line_vector = np.stack([(self.embedding[word] if word in self.embedding else self.unknown) for word in line])
-        target_list = [(self.embedding[word] if word in self.embedding else self.unknown) for word in line[1:]]
+
+        line2EmbeddingLine = [(self.embedding[word] if word in self.embedding else self.unknown) for word in line]
+        line_vector = np.stack(line2EmbeddingLine)
+        target_list = line2EmbeddingLine[1:]
         target_vector = np.stack(target_list + [self.EOS])
         
         input_tensor = torch.FloatTensor(line_vector)
@@ -52,5 +59,13 @@ class EmbeddingDataLoader(DataLoader):
             new_outp = F.pad(outp.unsqueeze(0).unsqueeze(0), (0,0,0,max_length-ss))
             inputs.append(new_inp.squeeze())
             outputs.append(new_outp.squeeze())
+
+        if len(inputs) != self.batch_size:
+            for _ in range(0, self.batch_size - len(inputs)):
+                inputs.append(torch.zeros(max_length  - ss, embedding_size ))
+
+        if len(outputs) != self.batch_size:
+            for _ in range(0, self.batch_size - len(outputs)):
+                outputs.append(torch.zeros(max_length  - ss, embedding_size ))
 
         return torch.stack(inputs), torch.stack(outputs)
