fb0baeb2600171ad461c9be5a9af564a09154b1a
yunseunghyun <rogve22@gmail.com>
2017-12-06 23:13:07 +0900

    [임베이딩-003] batch 사이즈 맞추는 로직 개

--- data_load/embedding_loader.py
+++ data_load/embedding_loader.py
@@ -4,9 +4,9 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+
 class EmbeddingDataset(Dataset):
-    
-    def __init__(self, filepath, embedding, vector_size, sampleSize = None):
+    def __init__(self, filepath, embedding, vector_size, sampleSize=None):
 
         with open(filepath, 'r') as f:
             if sampleSize is not None:
@@ -23,7 +23,7 @@ class EmbeddingDataset(Dataset):
         self.embedding = embedding
         print('total read line %d' % len(lines))
         print('cleaned lines %d' % len(self.cleaned))
-        
+
     def __getitem__(self, index):
         line = self.cleaned[index]
 
@@ -31,21 +31,21 @@ class EmbeddingDataset(Dataset):
         line_vector = np.stack(line2EmbeddingLine)
         target_list = line2EmbeddingLine[1:]
         target_vector = np.stack(target_list + [self.EOS])
-        
+
         input_tensor = torch.FloatTensor(line_vector)
         output_tensor = torch.FloatTensor(target_vector)
-        
+
         return input_tensor, output_tensor
-    
+
     def __len__(self):
         return len(self.cleaned)
 
+
 class EmbeddingDataLoader(DataLoader):
-    
     def __init__(self, *args, **kwargs):
         super(EmbeddingDataLoader, self).__init__(*args, **kwargs)
         self.collate_fn = self._collate_fn
-        
+
     def _collate_fn(self, batch):
         max_sample = max(batch, key=lambda x: x[0].size(0))
         max_length = max_sample[0].size(0)
@@ -55,17 +55,17 @@ class EmbeddingDataLoader(DataLoader):
         outputs = []
         for inp, outp in batch:
             ss = inp.size(0)
-            new_inp = F.pad(inp.unsqueeze(0).unsqueeze(0), (0,0,0,max_length-ss))
-            new_outp = F.pad(outp.unsqueeze(0).unsqueeze(0), (0,0,0,max_length-ss))
+            new_inp = F.pad(inp.unsqueeze(0).unsqueeze(0), (0, 0, 0, max_length - ss))
+            new_outp = F.pad(outp.unsqueeze(0).unsqueeze(0), (0, 0, 0, max_length - ss))
             inputs.append(new_inp.squeeze())
             outputs.append(new_outp.squeeze())
 
         if len(inputs) != self.batch_size:
             for _ in range(0, self.batch_size - len(inputs)):
-                inputs.append(torch.zeros(max_length  - ss, embedding_size ))
+                inputs.append(torch.zeros(max_length, embedding_size))
 
         if len(outputs) != self.batch_size:
             for _ in range(0, self.batch_size - len(outputs)):
-                outputs.append(torch.zeros(max_length  - ss, embedding_size ))
+                outputs.append(torch.zeros(max_length, embedding_size))
 
         return torch.stack(inputs), torch.stack(outputs)
