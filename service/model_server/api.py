3a8542a521cdc24139aa67eeb6c705aeb3f878a9
dreamgonfly <dreamgonfly@gmail.com>
2017-12-29 21:08:44 +0900

    Update api.py

--- service/model_server/api.py
+++ service/model_server/api.py
@@ -9,14 +9,92 @@ parentdir = os.path.dirname(os.path.dirname(currentdir))
 print(parentdir)
 sys.path.insert(0, parentdir) 
 
-from generate.generator import LineGenerator
-from train.trainer import DataReader2
-import torch
+import torch, gensim
+import numpy as np
+from data_load.embedding_loader import EmbeddingDataset, UniqueEmbeddingDataset, EmbeddingDataLoader
+from model.rnn import DeepLSTM
+from train.base_trainer import TrainValTrainer
+from generate.base_generator import LineEmbeddingGenerator
+from embedding.word2vec_model import ReplacingWord2VecModel
 
-rnn = torch.load(os.path.join(parentdir, 'train/output/saved_model.pkl'))
-data_reader = DataReader2()
-data_reader.load(os.path.join(parentdir, 'train/output/reader_params.pkl'))
-generator = LineGenerator(rnn, data_reader)
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+class Highway(nn.Module):
+    def __init__(self, num_features):
+        super(Highway, self).__init__()
+        self.fc1 = nn.Linear(num_features, num_features)
+        self.gate_layer = nn.Linear(num_features, num_features)
+        
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, inputs):
+        
+        normal_fc = self.tanh(self.fc1(inputs))
+        transformation_layer = self.sigmoid(self.gate_layer(inputs))
+        
+        carry_layer = 1 - transformation_layer
+        info_flow = normal_fc * transformation_layer + (1-transformation_layer)*inputs
+        return info_flow
+
+class DeepLSTM(nn.Module):
+    def __init__(self, wordvec_size=300, hidden_size=1024, linear_size=512,
+                 num_layers=3,
+                 highway_num_layers=2, bidirectional=False):
+        super(DeepLSTM, self).__init__()
+        
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size=wordvec_size, hidden_size=hidden_size,
+                            num_layers=num_layers, bidirectional=bidirectional,
+                            dropout=0.5, batch_first=True)
+        
+        self.hidden2out_1 = nn.Linear(hidden_size, hidden_size//2)
+        self.hidden2out_2 = nn.Linear(hidden_size, hidden_size//2)
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.batchnorm1 = nn.BatchNorm1d(hidden_size//2)
+        self.batchnorm2 = nn.BatchNorm1d(hidden_size//2)
+        self.batchnorm3 = nn.BatchNorm1d(hidden_size//2)
+        
+        self.highway = nn.ModuleList([Highway(hidden_size) for _ in range(highway_num_layers)])
+        
+        self.dense1 = nn.Linear(hidden_size, hidden_size//2)
+        self.dense2 = nn.Linear(hidden_size//2, wordvec_size)
+        
+    def forward(self, inputs):
+        # inputs : (batch_size, seq_size, embedding_size)
+        lstm_out, self.hidden = self.lstm(inputs, self.hidden)
+        self.last_out = lstm_out
+        self.relu_out = self.batchnorm1(self.relu(self.hidden2out_1(self.last_out)).view(-1, 512, inputs.size(1))).view(-1, inputs.size(1), 512)
+        self.tanh_out = self.batchnorm2(self.tanh(self.hidden2out_2(self.last_out)).view(-1, 512, inputs.size(1))).view(-1, inputs.size(1), 512)
+        self.concat = torch.cat([self.relu_out, self.tanh_out], dim=2)
+        
+        x = self.concat
+        for current_highway in self.highway:
+            x = current_highway(x)
+            
+        return self.dense2(self.batchnorm3(self.relu(self.dense1(x)).view(-1, 512, inputs.size(1))).view(-1, inputs.size(1), 512))
+
+    def init_hidden(self, batch_size, use_gpu=False):
+        hidden_shape = (self.num_layers, batch_size, self.hidden_size)
+        if use_gpu:
+            self.hidden = (Variable(torch.zeros(hidden_shape).cuda()),
+                Variable(torch.zeros(hidden_shape).cuda()))
+        else:
+            self.hidden = (Variable(torch.zeros(hidden_shape)),
+                Variable(torch.zeros(hidden_shape)))
+
+embedding = ReplacingWord2VecModel.load_from('only_lyrics_no_japanese_word2vec_100')
+model = torch.load('train/output/80_first_success.pth')
+model.eval()
+model = model.cpu()
+
+generator = LineEmbeddingGenerator(model=model, embedding=embedding, max_length=20, topn=5, use_gpu=False)
 
 app = Flask(__name__)
 CORS(app)
